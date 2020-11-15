7c11ef28b76a34e7a6f6bd29aec4050a4062c200
Artyom <artyom.arsalanov@gmail.com>
2020-11-16 00:31:42 +0800

    finished for now

--- InputOutput.cs
+++ InputOutput.cs
@@ -31,13 +31,6 @@ namespace Lab3
                 sw.Write(content);
             }
         }
-        static public bool IsInputCorrect(string number)
-        {
-            double num;
-            bool isInputCorrect = true;
-            if (!double.TryParse(number, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo, out num)) isInputCorrect = false;
-            return isInputCorrect;
-        }
         static public double GetDouble(string number="")
         {
             double num;
