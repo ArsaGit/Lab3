8645b41f59ff28860dd344675bb62c0a92ce94a3
Artyom <artyom.arsalanov@gmail.com>
2020-11-16 00:32:52 +0800

    finished for now

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            string str = "x-2;1.a11;1.1;11.4";
-            Function f= new Function();
-            f.GetInfo(str);
-            f.Print();
+            
+            
+            
+            
         }
     }
 }
