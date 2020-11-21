6f1e53dece4fddf905f692056b98d9e284bfe329
Artyom <artyom.arsalanov@gmail.com>
2020-11-21 20:04:04 +0800

    finishedv2

--- Program.cs
+++ Program.cs
@@ -7,16 +7,7 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            string data = InputOutput.ReadFromFile();
-            Function funct = new Function();
-            funct.GetInfo(data);
-            List<object> list = RPN.ParseText(funct.function);
-            foreach (object obj in list)
-                Console.WriteLine(obj);
-            Console.WriteLine();
-            Stack<object> rpn = RPN.ParseExpression(funct.function);
-
-            while (rpn.Count > 0) Console.WriteLine(rpn.Pop());
+            
 
         }
     }
